dff37d5b739cf02b944fad05ab06a78e09fdba09
Song.gunho <song6497@gmail.com>
2019-04-11 22:42:10 +0900

    ResponseEntity

--- src/main/java/com/ghsong/handlermethod/FileController.java
+++ src/main/java/com/ghsong/handlermethod/FileController.java
@@ -1,15 +1,28 @@
 package com.ghsong.handlermethod;
 
+import org.apache.tika.Tika;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.io.File;
+import java.io.IOException;
+
 @Controller
 public class FileController {
+
+    @Autowired
+    private ResourceLoader resourceLoader;
 
     @GetMapping("/file")
     public String fileUploadForm(Model model) {
@@ -26,5 +39,20 @@ public class FileController {
         return "redirect:/file";
     }
 
+
+    @GetMapping("/file/{filename}")
+    public ResponseEntity<Resource> fileDownload(@PathVariable String filename) throws IOException {
+        Resource resource = resourceLoader.getResource("classpath:" + filename);
+        File file = resource.getFile();
+
+        Tika tika = new Tika();
+        String mediaType = tika.detect(file);
+
+        return ResponseEntity.ok()
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachement; filename=\"" + resource.getFilename() + "\"")
+                .header(HttpHeaders.CONTENT_TYPE, mediaType)
+                .header(HttpHeaders.CONTENT_LENGTH, file.length() + "")
+                .body(resource);
+    }
 
 }
